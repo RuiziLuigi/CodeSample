dfec8adb4fc7de4643ccc220080f9e49588d5864
RuiziLuigi <luigi@project-projekt.com>
2015-02-08 14:26:09 +0900

    DeviceAppの初回コミット

---
+++ DeviceApp/DeviceApp/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pidText = new System.Windows.Forms.TextBox();
+            this.vidText = new System.Windows.Forms.TextBox();
+            this.driveText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.portText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "現在差し込まれたUSBメモリの情報を表示します";
+            // 
+            // dLabel
+            // 
+            this.dLabel.AutoSize = true;
+            this.dLabel.Location = new System.Drawing.Point(40, 100);
+            this.dLabel.Name = "dLabel";
+            this.dLabel.Size = new System.Drawing.Size(58, 12);
+            this.dLabel.TabIndex = 1;
+            this.dLabel.Text = "ドライブ名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "プロダクトid：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ベンダーid：";
+            // 
+            // pidText
+            // 
+            this.pidText.Location = new System.Drawing.Point(130, 137);
+            this.pidText.Name = "pidText";
+            this.pidText.ReadOnly = true;
+            this.pidText.Size = new System.Drawing.Size(145, 19);
+            this.pidText.TabIndex = 4;
+            this.pidText.TabStop = false;
+            // 
+            // vidText
+            // 
+            this.vidText.Location = new System.Drawing.Point(132, 177);
+            this.vidText.Name = "vidText";
+            this.vidText.ReadOnly = true;
+            this.vidText.Size = new System.Drawing.Size(145, 19);
+            this.vidText.TabIndex = 5;
+            this.vidText.TabStop = false;
+            // 
+            // driveText
+            // 
+            this.driveText.Location = new System.Drawing.Point(130, 97);
+            this.driveText.Name = "driveText";
+            this.driveText.ReadOnly = true;
+            this.driveText.Size = new System.Drawing.Size(145, 19);
+            this.driveText.TabIndex = 6;
+            this.driveText.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ポート情報：";
+            // 
+            // portText
+            // 
+            this.portText.Location = new System.Drawing.Point(130, 217);
+            this.portText.Name = "portText";
+            this.portText.ReadOnly = true;
+            this.portText.Size = new System.Drawing.Size(145, 19);
+            this.portText.TabIndex = 8;
+            this.portText.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 268);
+            this.Controls.Add(this.portText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.driveText);
+            this.Controls.Add(this.vidText);
+            this.Controls.Add(this.pidText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dLabel);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "UsbInfo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label dLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pidText;
+        private System.Windows.Forms.TextBox vidText;
+        private System.Windows.Forms.TextBox driveText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox portText;
+
+    }
+}
+
---
+++ DeviceApp/DeviceApp/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Management;
+using System.Threading;
+using Microsoft.Win32;
+
+namespace WindowsFormsApplication1
+{
+    delegate void UpdateView(string strVID, string strPID, string strHub);
+
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// イベント感知（デバイス変更時）
+        /// </summary>
+        private const int WM_DEVICECHANGE = 0x0219;
+
+        /// <summary>
+        /// デバイスイベント種別
+        /// </summary>
+        private enum DBT
+        {
+            /// <summary>
+            /// デバイス使用可能
+            /// </summary>
+            DBT_DEVICEARRIVAL = 0x8000,
+            /// <summary>
+            /// デバイス停止要求
+            /// </summary>
+            DBT_DEVICEQUERYREMOVE = 0x8001,
+            /// <summary>
+            /// デバイス停止要求失敗
+            /// </summary>
+            DBT_DEVICEQUERYREMOVEFAILED = 0x8002,
+            /// <summary>
+            /// デバイス停止中
+            /// </summary>
+            DBT_DEVICEREMOVEPENDING = 0x8003,
+            /// <summary>
+            /// デバイス停止
+            /// </summary>
+            DBT_DEVICEREMOVECOMPLETE = 0x8004,
+        }
+
+        /// <summary>
+        /// ボリューム情報構造体
+        /// </summary>
+        private struct DEV_BROADCAST_VOLUME
+        {
+            /// <summary>
+            /// サイズ
+            /// </summary>
+            public uint dbcv_size;
+            /// <summary>
+            /// 種別
+            /// </summary>
+            public uint dbcv_devicetype;
+            /// <summary>
+            /// 状態可否
+            /// </summary>
+            public uint dbcv_reserved;
+            /// <summary>
+            /// ボリューム名
+            /// </summary>
+            public uint dbcv_unitmask;
+
+            /// <summary>
+            /// パラメータセット
+            /// </summary>
+            /// <param name="a"></param>
+            /// <param name="b"></param>
+            /// <param name="c"></param>
+            /// <param name="d"></param>
+            public DEV_BROADCAST_VOLUME(uint a, uint b, uint c, uint d)
+            {
+                this.dbcv_size = a;
+                this.dbcv_devicetype = b;
+                this.dbcv_reserved = c;
+                this.dbcv_unitmask = d;
+            }
+        }
+
+
+        /// <summary>
+        /// ドライブ
+        /// </summary>
+        private char m_cDrive = '\0';
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Windowsイベント処理
+        /// </summary>
+        /// <param name="m">イベントメッセージ</param>
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                // デバイス変更時イベント
+                case WM_DEVICECHANGE:
+                    // 変更時イベント
+                    switch ((DBT)m.WParam.ToInt32())
+                    {
+                        // デバイス開始時処理
+                        case DBT.DBT_DEVICEARRIVAL:
+                            // 初回実行なら
+                            if (this.driveText.Text == string.Empty)
+                            {
+                                // とりあえず実行中にする   
+                                Application.UseWaitCursor = true;
+
+                                DEV_BROADCAST_VOLUME volume = (DEV_BROADCAST_VOLUME)Marshal.PtrToStructure(m.LParam, typeof(DEV_BROADCAST_VOLUME));
+                                // ビットが立っているところを取得
+                                this.m_cDrive = 'A';
+                                for (int iIdx = 0; iIdx < 25; iIdx++)
+                                {
+                                    if (volume.dbcv_unitmask >> iIdx == 1)
+                                    {
+                                        break;
+                                    }
+                                    this.m_cDrive++;
+                                }
+                                Thread thread = new Thread(GetUSBPort);
+                                thread.Start();
+                            }
+                            else
+                            {
+                                MessageBox.Show("起動につき一度しか使用できません。アプリケーションを再起動してください");
+                            }
+                            break;
+                        // デバイス停止時処理
+                        case DBT.DBT_DEVICEREMOVECOMPLETE:
+                            break;
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        /// <summary>
+        /// 画面更新
+        /// </summary>
+        /// <param name="strVID">ベンダーid</param>
+        /// <param name="strPID">プロダクトid</param>
+        /// <param name="strHub">ハブ</param>
+        private void updateScreen(string strVID,string strPID, string strHub)
+        {
+            if (this.InvokeRequired)
+            {
+                UpdateView uv = new UpdateView(updateScreen);
+                this.Invoke(uv, new string[] { strVID, strPID, strHub });
+            }
+            else
+            {
+                this.driveText.Text = this.m_cDrive.ToString();
+                this.vidText.Text = strVID;
+                this.pidText.Text = strPID;
+                this.portText.Text = strHub;
+                // 実行中を終了
+                Application.UseWaitCursor = false;
+            }
+        }
+
+        /// <summary>
+        /// USBからベンダーIDとプロダクトID取得
+        /// </summary>
+        private void GetUSBPort()
+        {
+            //ドライブが装着された時の処理を書く
+            // ドライバ認識まで待機
+            bool blFin = false;
+            while (!blFin)
+            {
+                Thread.Sleep(1000);
+                DriveInfo di = new DriveInfo(this.m_cDrive.ToString());
+                if (di.IsReady)
+                {
+                    blFin = true;
+                    Thread.Sleep(1000);
+                }
+            }
+            try
+            {
+                ManagementObjectSearcher searcher = null;
+                searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                        "SELECT * FROM Win32_LogicalDiskToPartition");
+
+                string strDeviceID = string.Empty;
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    ManagementObject buf = new ManagementObject(queryObj["Dependent"].ToString());
+                    if (buf["DeviceID"].ToString() == this.m_cDrive.ToString() + ":")
+                    {
+                        ManagementObject buf2 = new ManagementObject(queryObj["Antecedent"].ToString());
+                        strDeviceID = buf2["DeviceID"].ToString();
+                    }
+                }
+
+                searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                        "SELECT * FROM Win32_DiskPartition WHERE DeviceID='" + strDeviceID + "'");
+                string strDiskIdx = string.Empty;
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    strDiskIdx = queryObj["DiskIndex"].ToString();
+                    break;
+                }
+
+                searcher =
+                    new ManagementObjectSearcher("root\\CIMV2",
+                    "SELECT * FROM Win32_DiskDrive WHERE Index='" + strDiskIdx + "'");
+                string strPNPDeviceID = string.Empty;
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    strPNPDeviceID = queryObj["PNPDeviceID"].ToString();
+                    Console.WriteLine("DeviceID:" + queryObj["DeviceID"]);
+                    Console.WriteLine("PNPDeviceID:" + queryObj["PNPDeviceID"]);
+                }
+
+                searcher =
+                    new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_USBControllerDevice");
+
+
+                string strContainerID = string.Empty;
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    ManagementObject obj = new ManagementObject(queryObj["Dependent"].ToString());
+                    if (obj["DeviceID"].ToString() == strPNPDeviceID)
+                    {
+                        RegistryKey rKey = Registry.LocalMachine.OpenSubKey(
+                            "SYSTEM\\CurrentControlSet\\Enum\\" + strPNPDeviceID);
+
+                        strContainerID = (string)rKey.GetValue("ContainerID");
+                        rKey.Close();
+                        break;
+                    }
+                }
+                blFin = false;
+                RegistryKey rk = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Enum\\USB");
+                foreach (string strBuf in rk.GetSubKeyNames())
+                {
+                    RegistryKey rk2 = Registry.LocalMachine.OpenSubKey(
+                            "SYSTEM\\CurrentControlSet\\Enum\\USB\\" + strBuf);
+                    foreach (string strBuf2 in rk2.GetSubKeyNames())
+                    {
+                        RegistryKey rk3 = Registry.LocalMachine.OpenSubKey(
+                            "SYSTEM\\CurrentControlSet\\Enum\\USB\\" + strBuf + "\\" + strBuf2);
+                        if (rk3.GetValue("ContainerID").ToString() == strContainerID)
+                        {
+                            string[] strHardware = (string[])rk3.GetValue("HardwareID");
+                            StringBuilder sb = new StringBuilder();
+                            if (strHardware.Length > 0)
+                            {
+                                int iPid = strHardware[0].IndexOf("PID_");
+                                int iVid = strHardware[0].IndexOf("VID_");
+                                string strHubPort = (string)rk3.GetValue("LocationInformation");
+                                updateScreen(strHardware[0].Substring(iVid + 4, 4), strHardware[0].Substring(iPid + 4, 4), strHubPort);
+                            }
+                            blFin = true;
+                        }
+                        rk3.Close();
+                        if (blFin)
+                        {
+                            break;
+                        }
+                    }
+                    rk2.Close();
+                    if (blFin)
+                    {
+                        break;
+                    }
+                }
+                rk.Close();
+            }
+            catch (ManagementException e)
+            {
+                MessageBox.Show("エラー：" + e.Message);
+            }
+
+        }
+    }
+}
